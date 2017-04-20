ace4dd5dc3e3aa8f537f0fd397bc682d90a1f2c2
turbo25mike <turbo25mike@gmail.com>
2017-04-20 10:58:31 -0700

    url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUri: 'http://spdapi-432f0964-1.1a8d5e10.cont.dockerapp.io:5000/api/'
+  apiUri: 'http://spdapi-432f0964.dabafdfd.svc.dockerapp.io:5000/api/'
 };
